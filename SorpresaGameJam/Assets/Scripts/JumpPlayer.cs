2b7aa3cfd51179562d63d3011e4b8152898c6776
Marc Fabián i Vilà <marcfv97@gmail.com>
2019-01-21 23:16:24 +0100

    added jump anim on runner minigame

--- SorpresaGameJam/Assets/Scripts/JumpPlayer.cs
+++ SorpresaGameJam/Assets/Scripts/JumpPlayer.cs
@@ -5,26 +5,30 @@ using UnityEngine;
 public class JumpPlayer : MonoBehaviour {
 
     bool can_jump = true;
+    public float jump_amount = 250f;
+    public Sprite jumping;
+    public Sprite idle;
 
-	// Use this for initialization
-	void Start ()
-    {
-		
-	}
-	
-	// Update is called once per frame
+
 	void Update ()
     {
 		if(Input.GetKeyDown("up") && can_jump)
         {
-            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 270f));
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jump_amount));
             can_jump = false;
+        }
+        if(!can_jump)
+        {
+            GetComponent<SpriteRenderer>().sprite = jumping;
+        }
+        else
+        {
+            GetComponent<SpriteRenderer>().sprite = idle;
         }
 	}
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        
         if(collision.gameObject.tag == "Ground")
         {
             can_jump = true;
