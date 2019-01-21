55167fa3122439f5668736b9db591162c0f98fa1
xDragan <marcfv97@gmail.com>
2019-01-21 19:22:43 +0100

    runner minigame wip

---
+++ SorpresaGameJam/Assets/Runner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Runner : MonoBehaviour {
+
+    public float speed = 2.0f;
+    public float jump_force = 5.0f;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if(Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            transform.position += transform.up* jump_force*Time.deltaTime;
+        }
+	}
+}
