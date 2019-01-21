c45d4957e3678793768374ef8f93b52e4f2a102e
xDragan <marcfv97@gmail.com>
2019-01-21 14:03:43 +0100

    ball bouncing game

---
+++ SorpresaGameJam/Assets/scip.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scip : MonoBehaviour {
+
+    public GameObject board;
+
+    public float MoveSpeed = 40.0F;
+
+    private Rigidbody2D mRigidbody2D;
+
+    private float mMovementX = 0;
+    private float mMovementY = 0;
+
+    void Awake()
+    {
+        mRigidbody2D = this.GetComponent<Rigidbody2D>();
+
+
+        mMovementX = UnityEngine.Random.Range(10F, 20F);
+        mMovementY = UnityEngine.Random.Range(10F, 20F);
+
+        mRigidbody2D.velocity = new Vector2(mMovementX, mMovementY);
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        Vector3 normal = (board.transform.position - transform.position).normalized;
+
+        mRigidbody2D.velocity = Vector2.Reflect(mRigidbody2D.velocity, normal);
+    }
+}
