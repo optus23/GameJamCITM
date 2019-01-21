f009a0be83c479a89bf77cef8145746a099a29e6
Marc Gálvez <36281630+optus23@users.noreply.github.com>
2019-01-21 19:34:09 +0100

    Player jumps correctly

--- SorpresaGameJam/Assets/Scripts/JumpPlayer.cs
+++ SorpresaGameJam/Assets/Scripts/JumpPlayer.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class JumpPlayer : MonoBehaviour {
 
-    
+    bool can_jump = true;
 
 	// Use this for initialization
 	void Start ()
@@ -15,9 +15,20 @@ public class JumpPlayer : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-		if(Input.GetKeyDown("up"))
+		if(Input.GetKeyDown("up") && can_jump)
         {
             gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 270f));
+            can_jump = false;
         }
 	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+        if(collision.gameObject.tag == "Ground")
+        {
+            can_jump = true;
+            Debug.Log("Hi");
+        }
+    }
 }
