76df689c91b95bed7875e2216973ec24626bbee2
xDragan <marcfv97@gmail.com>
2019-01-21 12:56:44 +0100

    started with basic ui

---
+++ SorpresaGameJam/Assets/PickLock.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickLock : MonoBehaviour {
+    public Camera camera;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if(Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+            {
+               if(hit.collider.tag == "Clip")
+                {
+                    hit.collider.gameObject.transform.position = Input.mousePosition;
+                    Debug.Log("lockpit");
+                }
+            }
+        }
+	}
+}
