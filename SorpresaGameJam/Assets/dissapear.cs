3ba938efbdce0179d50a994e1db0e2bd92d44bdc
xDragan <marcfv97@gmail.com>
2019-01-22 11:48:24 +0100

    Props and open_close script for props

---
+++ SorpresaGameJam/Assets/dissapear.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dissapear : MonoBehaviour {
+
+    private void OnMouseDown()
+    {
+        Destroy(GameObject.Find("books_left"));
+    }
+
+}
---
+++ SorpresaGameJam/Assets/sprite_click.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sprite_click : MonoBehaviour {
+
+    public Sprite closed;
+    public Sprite open;
+    private bool is_open = false;
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnMouseDown()
+    {
+        is_open = !is_open;
+        SetSprite();
+    }
+    
+    void SetSprite()
+    {
+        if(is_open)
+        {
+            GetComponent<SpriteRenderer>().sprite = open;
+;       }
+        else
+        {
+            GetComponent<SpriteRenderer>().sprite = closed;
+        }
+    }
+}
