702371491c7d7159cb83cd0e245ded80e168c0ac
AAurelioGN <monsterlol64@gmail.com>
2019-01-22 09:22:51 +0100

    Update MainScene

--- SorpresaGameJam/Assets/Scripts/MainGame/RoomManager.cs
+++ SorpresaGameJam/Assets/Scripts/MainGame/RoomManager.cs
@@ -1,19 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class RoomManager : MonoBehaviour {
+   
+    public SpriteRenderer spriteRenderer ;
+    public Text roomText;
 
+    [Header("RoomsInfo")]
     public Sprite[] roomsSprites;
+    public string[] roomsNames;
+   
 
+    int currentRoom;
 	// Use this for initialization
 
 	void Start () {
-		
-	}
+        currentRoom = 0;
+        GameObject roomSprite = GameObject.Find("RoomSprite");
+        spriteRenderer = roomSprite.GetComponent<SpriteRenderer>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+
+
 }
