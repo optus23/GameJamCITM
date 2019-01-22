3f3fe62539c9056ba9b5f36a213e461a41528dd7
AAurelioGN <monsterlol64@gmail.com>
2019-01-22 11:46:20 +0100

    Updated mainScripts

--- SorpresaGameJam/Assets/Scripts/MainGame/RoomManager.cs
+++ SorpresaGameJam/Assets/Scripts/MainGame/RoomManager.cs
@@ -2,25 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
+using System.Xml;
 
 public class RoomManager : MonoBehaviour {
    
     private SpriteRenderer spriteRenderer ;
+    public GameObject propsGameObject;
     public Text roomText;
 
     [Header("RoomsInfo")]
     public string[] roomsNames;
     public Sprite[] roomsSprites;
-    public GameObject[] roomObjects;
+    public GameObject[] roomObjectsPrefabs;
+    private int currentRoom;
 
-    int currentRoom;
-	// Use this for initialization
+    private XmlDocument xmlDoc = new XmlDocument();
 
-	void Start () {
+    // Use this for initialization
+    void Start () {
         currentRoom = 0;
         GameObject roomSprite = GameObject.Find("RoomSprite");
         spriteRenderer = roomSprite.GetComponent<SpriteRenderer>();
+        xmlDoc.Load(Application.dataPath + "/Data/data.xml");
+        Save();
     }
 	
 	// Update is called once per frame
@@ -32,7 +36,44 @@ public class RoomManager : MonoBehaviour {
     {
         spriteRenderer.sprite = roomsSprites[roomId];
         roomText.text = roomsNames[roomId];
+        Object.Destroy(propsGameObject, 0.0f);
+        propsGameObject = Instantiate(roomObjectsPrefabs[currentRoom], new Vector3(0, 0, 0), Quaternion.identity);
     }
 
+    void Save()
+    {
+        XmlElement root = xmlDoc.DocumentElement;
+        XmlNode propsNode = root.FirstChild;
+        XmlNode roomNode = root.LastChild;
+
+        // Delete previous data
+        root.RemoveChild(propsNode);
+        root.RemoveChild(roomNode);
+
+        // Save current data
+        XmlElement roomElement = xmlDoc.CreateElement("room");
+        roomElement.SetAttribute("id", currentRoom.ToString());
+        XmlElement propsElement = xmlDoc.CreateElement("props");
+
+        foreach (Transform child in roomObjectsPrefabs[currentRoom].transform)
+        {
+            XmlElement propElement = xmlDoc.CreateElement("prop");
+            propElement.SetAttribute("active", child.gameObject.activeSelf.ToString());
+            propsElement.AppendChild(propElement);
+        }
+
+        root.AppendChild(propsElement);
+        root.AppendChild(roomElement);
+        xmlDoc.Save(Application.dataPath + "/Data/data.xml");
+    }
+
+    void Load()
+    {
+        XmlElement root = xmlDoc.DocumentElement;
+        XmlNode room = root.FirstChild;
+        currentRoom = int.Parse(room.Attributes["active"].Value);
+        ChangeRoom(currentRoom);
+
+    }
 
 }
